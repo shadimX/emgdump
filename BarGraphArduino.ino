80f1a5a30016f49f8f1fb81556bfa498b7fbdc7a
shadimX <shadimx@gmail.com>
2017-09-03 11:11:32 +0300

    Add files via upload

---
+++ BarGraphArduino.ino
@@ -0,0 +1,38 @@
+// reads analog input from the five inputs from your arduino board 
+// and sends it out via serial
+
+// variables for input pins and
+int analogInput=A0;
+  
+// variable to store the value 
+int value; 
+ 
+void setup()
+{
+  // declaration of pin modes
+      
+    pinMode(analogInput, INPUT);    
+  
+  
+  // begin sending over serial port
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  // read the value on analog input
+  
+    value = analogRead(analogInput);
+  
+
+  // print out value over the serial port
+
+   
+    Serial.println(value);
+   
+  
+  // wait for a bit to not overload the port
+  delay(100);
+}
+
+
